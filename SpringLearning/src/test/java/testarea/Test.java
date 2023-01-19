bd891378722d8497ece48cf7c23ca2e4394b3ead
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-01-19 21:20:47 +0000

    Initial Commit.

---
+++ SpringLearning/src/test/java/testarea/Test.java
@@ -0,0 +1,11 @@
+package testarea;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import main.Student;
+
+public class Test 
+{
+
+}
